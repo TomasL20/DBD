f16d6dcbf0414f023f557ed8a09a8c82564fd1c5
Romi <romina.palma@usach.cl>
2020-08-15 17:44:49 -0400

    Todas las tablas vacías
    Falta rellenar los atributos

---
+++ database/migrations/2020_08_15_214105_create_rol_permissions_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRolPermissionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rol_permissions', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rol_permissions');
+    }
+}
