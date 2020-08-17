b389048be43352499be55a1e5c06540c3d04abf5
Isaac <isaac.rojas@usach.cl>
2020-08-16 21:50:56 -0400

    Creados los seeder

---
+++ database/seeds/RolPermissionTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class RolPermissionTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
