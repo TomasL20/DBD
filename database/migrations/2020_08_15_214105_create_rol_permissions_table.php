ee8c629f95544e8d4964b207f83e7e44b2acb4b7
Esteban <esteban.silva.v@usach.cl>
2020-08-22 12:46:08 -0400

    Se solucionan unos pequeños detalles dentro de los comentarios agregando un poco de informacion a las migracionas

--- database/migrations/2020_08_15_214105_create_rol_permissions_table.php
+++ database/migrations/2020_08_15_214105_create_rol_permissions_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-
+//corresponde a la tabla intermedia formada por la tabla rol y la tabla permiso
 class CreateRolPermissionsTable extends Migration
 {
     /**
@@ -14,12 +14,13 @@ class CreateRolPermissionsTable extends Migration
     public function up()
     {
         Schema::create('rol_permissions', function (Blueprint $table) {
-            $table->id('id');
-            $table->unsignedBigInteger('idRol');
-            $table->unsignedBigInteger('idPer');
-
-            $table->foreign('idRol')->references('id')->on('rols');
-            $table->foreign('idPer')->references('id')->on('permissions');
+            $table->id('id'); //Identificador de la tabla
+            //Se crean las llavs foraneas
+            $table->unsignedBigInteger('idRol');//Identificador de la tabla Rol
+            $table->unsignedBigInteger('idPer');//Identificador de la tabla permisos
+            //Se referencian las llaves foraneas
+            $table->foreign('idRol')->references('id')->on('rols'); //corresponde a la llave foranea de la tabla rol
+            $table->foreign('idPer')->references('id')->on('permissions'); //corresponde a la llave forane a de la tabla permisos
             $table->timestamps();
         });
     }
