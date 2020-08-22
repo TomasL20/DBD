8027adc265e94afef2602d46d5e3bc6f1d1c03fe
Esteban <esteban.silva.v@usach.cl>
2020-08-22 15:08:12 -0400

    Se completa los comentarios de relaciones entre clases
    comentarios completados

--- app/UserRol.php
+++ app/UserRol.php
@@ -5,7 +5,7 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class UserRol extends Model
-{
+{   //se especifica el tipo de relaciones que posee la clase
     public function rol(){
         return $this->belongsTo(Rol::class);
     }
