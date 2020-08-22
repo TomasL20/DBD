d90795fa8efa822b1ea026bf9a251d38dc4192a2
Esteban <esteban.silva.v@usach.cl>
2020-08-22 15:03:26 -0400

    Se realizan comentarios sobre la relaciones de las clases
    falta rellenar

--- app/Permission.php
+++ app/Permission.php
@@ -6,9 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Permission extends Model
 {
+    //se colocan los campos los cuales se quieren llenar
     protected $fillable = [
         'description',
     ];
+    //se especifica el tipo de relacion que posee la clase
     public function rolPermission(){
         return $this->hasMany(RolPermission::class);
     }
