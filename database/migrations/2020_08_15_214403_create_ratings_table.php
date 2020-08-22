e8636f25c2e37da6b103407fc4fd764805e6b11e
Esteban <esteban.silva.v@usach.cl>
2020-08-22 12:32:21 -0400

    Se han realizado los comentarios de algunas tablas correspondeintes a las migraciones

--- database/migrations/2020_08_15_214403_create_ratings_table.php
+++ database/migrations/2020_08_15_214403_create_ratings_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-
+//Se hace la migracion de la tabla "ratings"
 class CreateRatingsTable extends Migration
 {
     /**
@@ -14,11 +14,11 @@ class CreateRatingsTable extends Migration
     public function up()
     {
         Schema::create('ratings', function (Blueprint $table) {
-            $table->id('id');
-            $table->unsignedBigInteger('idAd');
+            $table->id('id'); //Identificador de la tabla "ratings"
+            $table->unsignedBigInteger('idAd'); //Llave foranea correspondiente al identificador del anuncio
 
-            $table->integer('rate');
-            $table->string('comment');
+            $table->integer('rate'); // Corresponde a la evaluacion del anuncio numero 
+            $table->string('comment'); // corresponde al comentario de la evaluacion
             $table->timestamp('commentDate');
 
             $table->foreign('idAd')->references('id')->on('ads');
