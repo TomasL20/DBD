85fa828e4547141e64be7d7ad60ad409e7a98692
Esteban <esteban.silva.v@usach.cl>
2020-08-22 14:28:30 -0400

    Se ha comentado toda la seccion de factory
    Se comenta la seccion de factory, pero hay que aclarar unas dudas

--- database/factories/UserRolFactory.php
+++ database/factories/UserRolFactory.php
@@ -6,10 +6,12 @@ use App\UserRol;
 use Faker\Generator as Faker;
 
 $factory->define(UserRol::class, function (Faker $faker) {
+    //se especifica el tipo de dato
     $idRol = App\Rol::pluck('id')->toArray();
     $idUser = App\User::pluck('id')->toArray();
     return [
-        'idRol' => $faker->randomElement($idRol),
-        'idUser' => $faker->randomElement($idUser),
+        //se crean los fakers donde se especifican los tipos de datos que corresponde
+        'idRol' => $faker->randomElement($idRol), //se hace uso de este tipo de dato para generar un identificador del rol "idrol"
+        'idUser' => $faker->randomElement($idUser),//se hace uso de este tipo de dato para generar un identificador del rol "iduser"
     ];
 });
