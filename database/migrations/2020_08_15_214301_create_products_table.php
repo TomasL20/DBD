6a22e10a2a891a73f7ccccf3247bca1f61da1f00
Isaac <isaac.rojas@usach.cl>
2020-08-22 17:15:05 -0400

    Se dejan como nullable las llaves foraneas
    Solo de las que no son tabla intermedia. Aparte se cambió el formato de destroy de Ad.

--- database/migrations/2020_08_15_214301_create_products_table.php
+++ database/migrations/2020_08_15_214301_create_products_table.php
@@ -16,7 +16,7 @@ class CreateProductsTable extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id('id');
             //asignamos foraneas
-            $table->unsignedBigInteger('idCat');
+            $table->unsignedBigInteger('idCat')->nullable();
             
             
             $table->string('prodName'); //Nombre del produto 
