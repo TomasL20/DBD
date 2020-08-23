926b4375e8c4704491176467e84b963c74bc0b9e
Isaac <isaac.rojas@usach.cl>
2020-08-22 23:43:17 -0400

    Rutas de delete(ocultar) y restaurar

--- routes/web.php
+++ routes/web.php
@@ -32,14 +32,18 @@ Route::get('/category/{id}','CategoryController@show');
 Route::delete('/category/delete/{id}','CategoryController@destroy');
 Route::post('/category/create','CategoryController@store');
 Route::put('/category/update/{id}','CategoryController@update');
-Route::post('/category/restore/{id}','CategoryController@restore');
+Route::delete('/ad/sdelete/{id}','CategoryController@delete');
+Route::put('/ad/restore/{id}','CategoryController@restore');
+
 
 //Rutas de Order
 Route::get('/order/all','OrderController@index');
 Route::get('/order/{id}','OrderController@show');
 Route::delete('/order/delete/{id}','OrderController@destroy');
 Route::post('/order/create','OrderController@store');
-Route::put('/Order/update/{id}','OrderController@update');
+Route::put('/order/update/{id}','OrderController@update');
+Route::delete('/order/sdelete/{id}','OrderController@delete');
+Route::put('/order/restore/{id}','OrderController@restore');
 
 //Rutas de Payment
 Route::get('/payment/all','PaymentController@index');
@@ -47,6 +51,8 @@ Route::get('/payment/{id}','PaymentController@show');
 Route::delete('/payment/delete/{id}','PaymentController@destroy');
 Route::post('/payment/create','PaymentController@store');
 Route::put('/payment/update/{id}','PaymentController@update');
+Route::delete('/payment/sdelete/{id}','PaymentController@delete');
+Route::put('/payment/restore/{id}','PaymentController@restore');
 
 //Rutas de Permission
 Route::get('/permission/all','PermissionController@index');
@@ -54,6 +60,8 @@ Route::get('/permission/{id}','PermissionController@show');
 Route::delete('/permission/delete/{id}','PermissionController@destroy');
 Route::post('/permission/create','PermissionController@store');
 Route::put('/permission/update/{id}','PermissionController@update');
+Route::delete('/permission/sdelete/{id}','PermissionController@delete');
+Route::put('/permission/restore/{id}','PermissionController@restore');
 
 //Rutas de Product
 Route::get('/product/all','ProductController@index');
@@ -61,6 +69,8 @@ Route::get('/product/{id}','ProductController@show');
 Route::delete('/product/delete/{id}','ProductController@destroy');
 Route::post('/product/create','ProductController@store');
 Route::put('/product/update/{id}','ProductController@update');
+Route::delete('/product/sdelete/{id}','ProductController@delete');
+Route::put('/product/restore/{id}','ProductController@restore');
 
 //Rutas de Rating
 Route::get('/rating/all','RatingController@index');
@@ -68,6 +78,8 @@ Route::get('/rating/{id}','RatingController@show');
 Route::delete('/rating/delete/{id}','RatingController@destroy');
 Route::post('/rating/create','RatingController@store');
 Route::put('/rating/update/{id}','RatingController@update');
+Route::delete('/rating/sdelete/{id}','RatingController@delete');
+Route::put('/rating/restore/{id}','RatingController@restore');
 
 //Rutas de Record
 Route::get('/record/all','RecordController@index');
@@ -75,6 +87,8 @@ Route::get('/record/{id}','RecordController@show');
 Route::delete('/record/delete/{id}','RecordController@destroy');
 Route::post('/record/create','RecordController@store');
 Route::put('/record/update/{id}','RecordController@update');
+Route::delete('/record/sdelete/{id}','RecordController@delete');
+Route::put('/record/restore/{id}','RecordController@restore');
 
 //Rutas de Rol
 Route::get('/rol/all','RolController@index');
@@ -82,6 +96,8 @@ Route::get('/rol/{id}','RolController@show');
 Route::delete('/rol/delete/{id}','RolController@destroy');
 Route::post('/rol/create','RolController@store');
 Route::put('/rol/update/{id}','RolController@update');
+Route::delete('/rol/sdelete/{id}','RolController@delete');
+Route::put('/rol/restore/{id}','RolController@restore');
 
 //Rutas de RolPermission
 Route::get('/rolpermission/all','RolPermissionController@index');
@@ -89,6 +105,8 @@ Route::get('/rolpermission/{id}','RolPermissionController@show');
 Route::delete('/rolpermission/delete/{id}','RolPermissionController@destroy');
 Route::post('/rolpermission/create','RolPermissionController@store');
 Route::put('/rolpermission/update/{id}','RolPermissionController@update');
+Route::delete('/rolpermission/sdelete/{id}','RolPermissionController@delete');
+Route::put('/rolpermission/restore/{id}','RolPermissionController@restore');
 
 //Rutas de User
 Route::get('/user/all','UserController@index');
@@ -96,6 +114,8 @@ Route::get('/user/{id}','UserController@show');
 Route::delete('/user/delete/{id}','UserController@destroy');
 Route::post('/user/create','UserController@store');
 Route::put('/user/update/{id}','UserController@update');
+Route::delete('/user/sdelete/{id}','UserController@delete');
+Route::put('/user/restore/{id}','UserController@restore');
 
 //Rutas de UserRol
 Route::get('/userrol/all','UserRolController@index');
@@ -103,3 +123,5 @@ Route::get('/userrol/{id}','UserRolController@show');
 Route::delete('/userrol/delete/{id}','UserRolController@destroy');
 Route::post('/userrol/create','UserRolController@store');
 Route::put('/userrol/update/{id}','UserRolController@update');
+Route::delete('/userrol/sdelete/{id}','UserRolController@delete');
+Route::put('/userrol/restore/{id}','UserRolController@restore');
