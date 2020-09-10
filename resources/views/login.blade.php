846152756893ccf95106ba9f52e3b28aa7276ecc
Esteban <esteban.silva.v@usach.cl>
2020-09-10 00:08:00 -0300

    Se crea vista Login
    Se ha creado la vista login para poder iniciar sesion, re direcciona a crear cuenta

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -9,76 +9,108 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="{{ asset('css/vistaone.css') }}">
     <link href="https://fonts.googleapis.com/css2?family=Black+Ops+One&display=swap" rel="stylesheet"> 
-    <title>Hello, world!</title>
+    <title>Te presto el login</title>
   </head>
   
+
   <style>
 
-      .propiedades-cuerpo{
+
+     
+
+      .formulario{
           background-color: #353535;
           text-align:center;
+          padding: 15px;
       }
+
+      h1{
+          color:#d9d9d9;
+          text-align: center;
+      }
+      p{
+          color:#d9d9d9;
+          text-align: center;
+      }
+      
+      .input-contenedor{
+        margin-top: 15px;
+      }
+      
       .logo{
         object-position: center top;
         margin-top: 30px;
         margin-bottom: 40px;
       }
+
+
       .upper-name{
             color:#3C6E71;
             margin:0;
             margin-top:100px;
 
       }
+
       .inputs{
         margin-bottom: 30px;
         border-radius: 20px;
         inline-size: 20%;
       }
+
+
       .labels{
         color:#ffffff;
         margin-bottom: 0px;
         display:block;
       }
+
+
       .btn-secondary{
-        
+        margin-top: 15px;
+        margin-bottom: 15px;
         border:solid 1px;
         padding:8px;
         border-color:#fff;
         border-radius: 20px;
         background-color:#3C6E71;
-        
       }
-  </style>
-  <body class = "propiedades-cuerpo">
-    
-    <div class = "container">
 
+
+  </style>
+  <body class = "formulario">
+
+    <div class = "contenedor">
         <div class = "logo">  
           <img src = "https://cdn.discordapp.com/attachments/621506339725901854/753388089044631652/unknown.png" alt = "logo"></img>
         </div>
-        <div class = "register">
-          <div>
-            <label name = "mailLabel" class = "labels">Ingresa tu correo electrónico:</label>
-            <input name = "mailInput" class = "inputs" type = "text" placeholder = "E-mail"></input>
+
+        <h1>Iniciar Sesión</h1>
+
+        <div class = "login">
+
+          <div class="input-contenedor">
+            <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-person-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd" d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+            </svg>
+            <input type="text" placeholder = "Correo Electrónico">
           </div>
-          <div>
-            <label name = "userLabel" class = "labels">Ingresa tu nombre completo:</label>
-            <input name = "userInput" class = "inputs" type = "text" placeholder = "Nombre completo"></input>
-          </div>
-          <div>
-            <label name = "usernameLabel" class = "labels">Ingresa tu nombre de usuario:</label>
-            <input name = "usernameInput" class = "inputs" type = "text" placeholder = "Nombre de usuario"></input>
-          </div>
-          <div>
-            <label name = "passwordLabel" class = "labels">Ingresa tu nombre completo:</label>
-            <input name = "passwordInput" class = "inputs" type = "password" placeholder = "Contraseña"></input>
-          </div>
-          <div>
-            <button name = "registerbtn" type="button" class="btn btn-secondary">Crear mi cuenta</button>
+
+
+          <div class="input-contenedor">
+            <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-lock-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+            <path d="M2.5 9a2 2 0 0 1 2-2h7a2 2 0 0 1 2 2v5a2 2 0 0 1-2 2h-7a2 2 0 0 1-2-2V9z"/>
+            <path fill-rule="evenodd" d="M4.5 4a3.5 3.5 0 1 1 7 0v3h-1V4a2.5 2.5 0 0 0-5 0v3h-1V4z"/>
+            </svg>
+            <input type="password" placeholder = "Contraseña">
           </div>
           
-          
-          
+          <div>
+            <button name = "registerbtn" type="button" class="btn btn-secondary">Iniciar Sesión</button>
+          </div>
+
+          <p>Al registrarte, aceptas nuestras Condiciones de uso y Política de privacidad.</p>
+          <p>¿No tienes cuenta?<a class="link" href="register.blade.php">Crear Cuenta</a></p>
+        
         </div>
         
     </div>
