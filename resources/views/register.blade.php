f1b0c0718f11aa1d7497ef34b4df48063f813e85
Isaac <isaac.rojas@usach.cl>
2020-09-10 18:27:34 -0300

    Se modifica devuelta la vista register

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -22,18 +22,20 @@
           text-align:center;
           padding: 15px;
       }
-
-      h1{
-          color:#d9d9d9;
-          text-align: center;
+      .normalText{
+        color:#d9d9d9;
       }
-      p{
+      .end{
           color:#d9d9d9;
-          text-align: center;
+          margin-right:10px;
+          display:inline-flex;
       }
       
-      .input-contenedor{
+      .inputs{
         margin-top: 15px;
+        margin-bottom: 30px;
+        border-radius: 20px;
+        inline-size: 20%;
       }
       
       .logo{
@@ -42,27 +44,11 @@
         margin-bottom: 40px;
       }
 
-
-      .upper-name{
-            color:#3C6E71;
-            margin:0;
-            margin-top:100px;
-
-      }
-
-      .inputs{
-        margin-bottom: 30px;
-        border-radius: 20px;
-        inline-size: 20%;
-      }
-
-
       .labels{
         color:#ffffff;
         margin-bottom: 0px;
         display:block;
       }
-
 
       .btn-secondary{
         margin-top: 15px;
@@ -72,57 +58,62 @@
         border-color:#fff;
         border-radius: 20px;
         background-color:#3C6E71;
+        inline-size: 10%;
       }
-
 
   </style>
   <body class = "formulario">
 
     <div class = "contenedor">
         <div class = "logo">  
-          <img src = "https://cdn.discordapp.com/attachments/621506339725901854/753388089044631652/unknown.png" alt = "logo"></img>
+          <img src = "{{asset('/images/logoDBD.png')}}" alt = "logo"></img>
         </div>
 
-        <h1>Iniciar Sesión</h1>
+        <h1 class = "normalText">Registrarse</h1>
 
         <div class = "login">
 
-          <div class="input-contenedor">
-            <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-envelope-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+          <div>
+            <label name = "mailLabel" class = "labels">Ingresa tu correo electrónico:</label>
+            <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-envelope-fill normalText" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
             <path fill-rule="evenodd" d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555zM0 4.697v7.104l5.803-3.558L0 4.697zM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757zm3.436-.586L16 11.801V4.697l-5.803 3.546z"/>
             </svg>
-            <input type="text" placeholder = "Correo Electrónico">
+            <input type="text" placeholder = "Correo Electrónico" class = "inputs">
           </div>
 
 
-          <div class="input-contenedor">
-            <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-person-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+          <div>
+            <label name = "mailLabel" class = "labels">Ingresa tu nombre completo:</label>
+            <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-person-fill normalText" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
             <path fill-rule="evenodd" d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
             </svg>
-            <input type="text" placeholder = "Nombre Completo">
+            <input type="text" placeholder = "Nombre Completo" class = "inputs">
           </div>
 
-          <div class="input-contenedor">
-            <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-person-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+          <div>
+            <label name = "mailLabel" class = "labels">Ingresa tu nombre de usuario:</label>
+            <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-person-fill normalText" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
             <path fill-rule="evenodd" d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
             </svg>
-            <input type="text" placeholder = "Nombre de usuario">
+            <input type="text" placeholder = "Nombre de usuario" class = "inputs">
           </div>
 
-          <div class="input-contenedor">
-            <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-lock-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+          <div>
+            <label name = "mailLabel" class = "labels">Ingresa tu contraseña:</label>
+            <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-lock-fill normalText" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
             <path d="M2.5 9a2 2 0 0 1 2-2h7a2 2 0 0 1 2 2v5a2 2 0 0 1-2 2h-7a2 2 0 0 1-2-2V9z"/>
             <path fill-rule="evenodd" d="M4.5 4a3.5 3.5 0 1 1 7 0v3h-1V4a2.5 2.5 0 0 0-5 0v3h-1V4z"/>
             </svg>
-            <input type="password" placeholder = "Contraseña">
+            <input type="password" placeholder = "Contraseña" class = "inputs">
           </div>
           
           <div>
             <button name = "registerbtn" type="button" class="btn btn-secondary">Crear Cuenta</button>
           </div>
 
-          <p>Al registrarte, aceptas nuestras Condiciones de uso y Política de privacidad.</p>
-          <p>¿Ya tienes cuenta?<a class="link" href="login.blade.php">Iniciar Sesión</a></p>
+          <p class = "normalText">Al registrarte, aceptas nuestras Condiciones de uso y Política de privacidad.</p>
+          <p class = "end">¿Ya tienes cuenta?</p>
+          <a class="link" href="login.blade.php">Iniciar Sesión</a>
         
         </div>
         
